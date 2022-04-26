f910dd922e768ceecb2d3cdbec6c31c655ec65a4
ayaplorenzjillian <100033705+ayaplorenzjillian@users.noreply.github.com>
2022-04-26 10:22:24 +0800

    Add files via upload

---
+++ LT5.1/script.js
@@ -0,0 +1,26 @@
+function talkid(obj) {
+    "use strict";
+    if (obj.id === 'bratz-1') {
+        const myElement = document.getElementById('cloe-talk');
+         let myElement.style.display = "block";
+}  if (obj.id === 'bratz-2') {
+        const myElement = document.getElementById('sasha-talk');
+          let myElement.style.display = "block";       
+}  if (obj.id === 'bratz-3') {
+        const myElement = document.getElementById('yasmin-talk');
+          let  myElement.style.display = "block";
+    }
+}
+
+function hideid(obj) {
+    if (obj.id === 'bratz-1') {
+        const myElement = document.getElementById('cloe-talk');
+           let myElement.style.display = "none";
+ } if (obj.id === 'bratz-2') {
+        const myElement = document.getElementById('sasha-talk');
+           let myElement.style.display = "none";
+ } if (obj.id === 'bratz-3') {
+        const myElement = document.getElementById('yasmin-talk');
+           let myElement.style.display = "none";
+    }
+}
