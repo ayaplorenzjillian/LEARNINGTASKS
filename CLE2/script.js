dee1b5a4182a43b7116b2047f2e8dc6fcd26eeca
ayaplorenzjillian <100033705+ayaplorenzjillian@users.noreply.github.com>
2022-05-16 20:24:35 +0800

    Add files via upload

---
+++ CLE2/script.js
@@ -0,0 +1,18 @@
+function openLink() {
+ window.open("MYHOBBIES.html")
+}
+function openLink() {
+window.open("https://ayap-mid.herokuapp.com/")
+}
+function openLink() {
+window.open("https://ayap-lt-2.herokuapp.com/")
+}
+function openLink() {
+window.open("https://lt-3-ayap.herokuapp.com/")
+}
+function openLink() {
+window.open("https://learningtask4.herokuapp.com/")
+}
+function openLink() {
+window.open("https://github.com/ayaplorenzjillian/LEARNINGTASKS/tree/main/LT5.1")
+}
