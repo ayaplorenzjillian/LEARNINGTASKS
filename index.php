8a6b0004e422a00a23d91f24e29f8e6fcaa0fefc
ayaplorenzjillian <100033705+ayaplorenzjillian@users.noreply.github.com>
2022-05-16 20:22:41 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title> MY PERSONAL BLOG </title>
+    <h1><center> MY PERSONAL BLOG </center></h1>
+    <link rel="stylesheet" "text/css" href="stylesheet.css">
+    <script src= script.js> </script>
+    </head>
+    <body>
+ <div id="wrapper">
+            <div id="box-one">
+                <head>
+          <title> MY WORKS AND HOBBIES</title>
+         </head>
+
+                <a href= "MYHOBBIES.html" target="_blank"> MY HOBBIES </a> 
+                <a href= "https://ayap-mid.herokuapp.com/" target="_blank"> CLE 1 </a>
+                <a href= "https://ayap-lt-2.herokuapp.com/" target="_blank"> LEARNING TASK 2 </a>
+                <a href= "https://lt-3-ayap.herokuapp.com/" target="_blank"> LEARNING TASK 3 </a>
+                <a href= "https://learningtask4.herokuapp.com/" target="_blank"> LEARNING TASK 4 </a>
+                <a href= "https://github.com/ayaplorenzjillian/LEARNINGTASKS/tree/main/LT5.1" target="_blank"> LEARNING TASK 5 </a>
+
+        <div id="wrapper">
+            <div id="box-two">
+            <h3> About Me </h3>
+            <center> <img src="aboutme.jpg" width="350" height="400"></center>
+                <p> Hello! I'm Lorenz Jillian Ayap, I live in Binalbagan, Negros Occidental. I am a 19 year old student that studies Bachelor of Science in Information Tchnology in University of St La Salle. I graduated my JHS and SHS in Binalbagan Catholic College, I took ABM when I was in my Senior Year.</p>
+                 <p> I really love drinking coffee every morning, it's like when I drunk a cup of coffee my whole day is complete. </p>
+            </div>     
+        </div>
+       </div>     
+        </div>
+          </body>
+</html> 
