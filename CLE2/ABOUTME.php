357800d198239e83b3b409d3b086b4c2cc1b5db6
ayaplorenzjillian <100033705+ayaplorenzjillian@users.noreply.github.com>
2022-05-21 14:00:23 +0800

    Add files via upload

---
+++ CLE2/ABOUTME.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> ALL ABOUT ME </title>
+   <link rel="stylesheet" "text/css" href="stylesheet.css">
+    </head>
+    <body>     
+        <?php
+ <div id="wrapper">
+            <div id="box-two">
+                
+         echo "<h1><center> About Me </center></h1>";
+            <center> <img src="aboutme.jpg" width="370" height="400"></center>
+               echo "<p> Hello! I'm Lorenz Jillian Ayap, I live in Binalbagan, Negros Occidental. I am a 19 year old student that studies Bachelor of Science in Information Technology in University of St La Salle. I graduated my JHS and SHS in Binalbagan Catholic College, I took ABM when I was in my Senior Year.</p>";
+                 echo "<p> I really love drinking coffee every morning, it's like when I drunk a cup of coffee my whole day is complete. </p>";
+            
+           echo "<h1> <center> My Hobbies </center></h1>";
+        
+            <div id="wrapper">
+            <div id="box-one">
+            
+        echo "<p> READING BOOKS </p>";
+
+            <center> <img src="hih.jpg"width="350" height="300"></center>
+            echo "<p> I really love reading books!! One of my faves are He is into Her, Twilight (All book versions), and all Harry Potter books! </p>";
+        </div>
+         </div>   
+        
+            <div id="wrapper">
+            <div id="box-two">
+        echo "<p> WATCHING MY FAVORITE SERIES AND SHOWS </p>"
+    
+            <center> <img src="marvel.jpg" width="500" height="250"></center>
+             <center> <img src="ST.jpg" width="500" height="250"></center>
+            <center> <img src="friends.jpg" width="500" height="250"></center>
+             echo "<p> One of My hobbies is Watching my Favorites Series and Movies; Friends (Series), Gilmore Girls (Series), Stranger Things (Series), 10 Things I Hate About You, and all Marvel Movies and Series are my Favorites.</p>";
+            </div>     
+        </div>
+        
+        <div id="wrapper">
+            <div id="box-three">
+        <p> LISTENING TO SONGS </p>
+         
+             <center> <img src="tswift.jpg" width="500" height="300"></center>    
+             <center> <img src="queen.jpg" width="500" height="300"></center>
+                echo "<p> My Favorite Artists are Ariana Grande, Bee Gees, Queen, Kitchie Nadal, and TAYLOR SWIFT!!!</p>";
+             </div> 
+            </div>     
+        </div>
+        </div>
+       ?>     
+</body>
